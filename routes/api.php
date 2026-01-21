191440fcd250e1b2175101a48c5336e0d036a30a
João Vitor <joao.santos@grupohne.com.br>
2026-01-21 19:13:50 -0300

    feature: Optimization in api routes, add of apiResource route and strutural organization

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\LoginController;
 
-Route::middleware(["auth:sanctum"])->get("/user", function (Request $request) {
-    return $request->user();
-});
+// Resources routes
+Route::apiResource("tasks", TaskController::class)->middleware("auth:sanctum");
+Route::apiResource("users", UserController::class)->middleware("auth:sanctum");
 
+// Individual routes
 Route::get("/", function() {
     return response()->json(["message" => "API is running"]);
 });
 
+// Group routes
 Route::group(["prefix" => "auth"], function() {
     Route::post("/login", [LoginController::class, "login"]);
-});
-
-Route::group(["prefix" => "tasks", "middleware" => ["auth:sanctum"]], function() {
-    Route::get("/", [TaskController::class, "index"]);
-    Route::post("/", [TaskController::class, "store"]);
-    Route::get("/{task}", [TaskController::class, "show"]);
-    Route::put("/{task}", [TaskController::class, "update"]);
-    Route::delete("/{task}", [TaskController::class, "destroy"]);
-});
-
-Route::group(["prefix" => "users", "middleware" => ["auth:sanctum"]], function() {
-    Route::get("/", [UserController::class, "index"]);
-    Route::post("/", [UserController::class, "store"]);
-    Route::get("/{user}", [UserController::class, "show"]);
-    Route::put("/{user}", [UserController::class, "update"]);
-    Route::delete("/{user}", [UserController::class, "destroy"]);
 });
