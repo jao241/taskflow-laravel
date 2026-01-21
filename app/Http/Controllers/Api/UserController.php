4a7f74f4a28bba82f955cd3a33a59bfbb9b5af9b
João Vitor <joao.santos@grupohne.com.br>
2026-01-21 19:12:35 -0300

    feature: Create userPolicy and add authorization in update method in user controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -87,6 +87,8 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
+        $this->authorize('update', $user);
+
         DB::beginTransaction();
 
         try {
