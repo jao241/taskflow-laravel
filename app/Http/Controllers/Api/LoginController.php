59550c65aa215a81ad9fe9ad86852d2aa6df7555
João Vitor <joao.santos@grupohne.com.br>
2026-01-21 19:11:17 -0300

    feature: Update in login method and add LoginRequest validator

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Exception;
@@ -10,31 +11,24 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    public function login(Request $request) {
-        try {
-            $request->validate([
-                'email' => 'required|email',
-                'password' => 'required|string',
-            ]);
+    public function login(LoginRequest $request) {
+        $data = $request->validated();
 
-            $user = User::where('email', $request->email)
-            ->first();
+        $user = User::where('email', $data['email'])
+        ->first();
 
-            if (!$user) {
-                return response()->json(["message" => "User not found"], 404);
-            }
-
-            $correctPassword = Hash::check($request->password, $user->password);
-
-            if (!$correctPassword) {
-                return response()->json(["message" => "Invalid credentials"], 401);
-            }
-
-            $accessToken = $user->createToken('authToken')->plainTextToken;
-
-            return response()->json(["message" => $accessToken], 200);
-        } catch (Exception $error) {
-            return response()->json([["message" => "Login failed", "error" => $error->getMessage()]], 500);
+        if (!$user) {
+            return response()->json(["message" => "User not found"], 404);
         }
+
+        $correctPassword = Hash::check($data['password'], $user->password);
+
+        if (!$correctPassword) {
+            return response()->json(["message" => "Invalid credentials"], 401);
+        }
+
+        $accessToken = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(["message" => $accessToken], 200);
     }
 }
